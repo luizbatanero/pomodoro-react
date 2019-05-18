c07c21419cde50d8da90433ab47b62a7e21fbb97
Luiz Batanero <luizbatanero@gmail.com>
2019-05-18 20:47:57 -0300

    controls css

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import './Controls.css';
 
 const Controls = ({ start, reset, pause, running, interval, time }) => {
   return (
     <div className="Controls">
       {running === false ? (
-        <button onClick={start}>
-          {time === 0 ? 'Restart timer' : 'Start timer'} <span>[space]</span>
+        <button onClick={start} className="start">
+          {time === 0 ? 'Restart timer' : 'Start timer'}
         </button>
       ) : (
-        <>
-          <button onClick={reset}>
-            Reset <span>[esc]</span>
+        <div>
+          <button onClick={reset} className="reset">
+            Reset
           </button>
-          <button onClick={pause}>
-            {interval ? 'Pause' : 'Resume'} <span>[space]</span>
+          <button onClick={pause} className={interval ? 'pause' : 'resume'}>
+            {interval ? 'Pause' : 'Resume'}
           </button>
-        </>
+        </div>
       )}
     </div>
   );
