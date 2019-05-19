11105d3daa8edaf5d43cbad19bcf26c4d145e31e
Luiz Batanero <luizbatanero@gmail.com>
2019-05-19 15:15:48 -0300

    Updates

---
+++ precache-manifest.33c468183498c67bc9d864d6f7c5e289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59e27a38746b90d2587560ed8b9ab1c",
+    "url": "/pomodoro-react/index.html"
+  },
+  {
+    "revision": "030d106142cf50008dc6",
+    "url": "/pomodoro-react/static/css/main.158d29c8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/pomodoro-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "030d106142cf50008dc6",
+    "url": "/pomodoro-react/static/js/main.2e26816c.chunk.js"
+  },
+  {
+    "revision": "e8d36cf3d72ad2a16867",
+    "url": "/pomodoro-react/static/js/runtime~main.31035ebe.js"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Pomodoro from './containers/Pomodoro';
-
-ReactDOM.render(<Pomodoro />, document.getElementById('root'));
